2778b6fafa6b633819882cd1adb5a12d6fc638de
Brian Arnold <brianarnold@Brians-MBP.attlocal.net>
2021-03-13 14:51:24 -0500

    Homework done!
    -Player.cs added
    -Desired changes have been made
    -See comments at bottom of Test.cs for specifics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void DamageAllEnemies()
+    {
+        foreach (Enemy enemy in Test.enemies)
+        {
+            if (enemy.enemyName == "Goomba")
+            {
+                enemy.TakeDamage(99);
+            }
+            else
+            {
+                enemy.TakeDamage(5);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DamageAllEnemies();
+        }
+    }
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,41 +7,40 @@ public class Test : MonoBehaviour
     public Enemy koopaPrefab;
     public Enemy bobombPrefab;
 
-    private List<Enemy> enemies = new List<Enemy>();
+    public static List<Enemy> enemies = new List<Enemy>();
     private int enemyCount = 1;
+
+    private void Awake()
+    {
+        Debug.Log("PRESS 'A' TO ATTACK!");
+
+        void PopulateEnemyList()
+        {
+            for (int i = 0; i < enemyCount; i++)
+            {
+                enemies.Add(Instantiate(goombaPrefab));
+                enemies.Add(Instantiate(koopaPrefab));
+                enemies.Add(Instantiate(bobombPrefab));
+            }
+            enemies.AddRange(FindObjectsOfType<Enemy>());
+        }
+
+        PopulateEnemyList();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        PopulateEnemyList();
-        DamageAllEnemies();
+
     }
 
     //Homework for Brian! Do this in an awake function so it happens before the player tries to attack
-    void PopulateEnemyList()
-    {
-        for (int i = 0; i < enemyCount; i++)
-        {
-            enemies.Add(Instantiate(goombaPrefab));
-            enemies.Add(Instantiate(koopaPrefab));
-            enemies.Add(Instantiate(bobombPrefab));
-        }
-        enemies.AddRange(FindObjectsOfType<Enemy>());
-    }
+    //BRIAN: DONE!
+
 
     //Homework for Brian! Make a player class, and move this function into their start function!
-    void DamageAllEnemies()
-    {
-        foreach (Enemy enemy in enemies)
-        {
-            if (enemy.enemyName == "Goomba")
-            {
-                enemy.TakeDamage(99);
-            }
-            else
-            {
-                enemy.TakeDamage(5);
-            }
-        }
-    }
+    //BRIAN: I actually put the DamageAllEnemies() function outside the start function.
+    //BRIAN: I called DamageAllEnemies() in the update function using an if statement to determine if the player is pressing "A"
+
+    //BRIAN: I also created a new Player prefab in the Unity editor and attached the PlayerController script to it.
 }
